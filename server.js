6ccc649c7e0818803cda5c9ba6da20312e54a129
Don Newman <dnewman@talentpath.com>
2019-10-17 08:56:58 -0500

    ERPROTO error

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-// const http = require("http");
-// const hostname = "127.0.0.1";
-// const url = require("url");
-// const port = 3000;
+const https = require("https");
+const hostname = "127.0.0.1";
+const url = require("url");
+const port = 3000;
+
+const data = JSON.stringify({
+  quote: "quote"
+});
+const options = {
+  hostname: "127.0.0.1",
+  port: 3000,
+  path: "/",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Content-Length": data.length
+  }
+};
+
 const array = [
   "A good ol movie quote",
   "Honey? Wheeeere's my super suit??",
@@ -15,66 +30,38 @@ const array = [
   "You are about to witness the strength of street knowledge"
 ];
 let randomQuote = array[Math.floor(Math.random() * 9)];
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader("content-type", "text/plain");
-//   res.end(randomQuote);
-// });
-// server.listen(port, hostname, () => {
-//   console.log("server successfully listening at port" + port);
-//   const data = JSON.stringify({
-//     quote: randomQuote
-//   });
-//   const options = {
-//     host: "127.0.0.1",
-//     path: "/",
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Content-Length": data.length
-//     }
-//   };
-//   const req = http.request(options, res => {
-//     res.on("data", function(data) {
-//       process.stdout.write(data);
-//     });
-//   });
+const requestHandler = (req, res) => {
+  console.log(req.url);
+  res.end("Helo w0rld");
+};
+const server = https.createServer(requestHandler);
 
-//   req.write(data);
-//   req.end();
-// });
-// const httpServer = http.createServer(function(req, res) {
-//   const reqUrlString = req.url;
-//   const pathName = url.parse(reqUrlString, true, false).pathname;
-//   if ("/login" == pathName) {
-//     const method = req.method;
-//     if ("POST" == method) {
-//       req.on("data", function(chunk) {
-//         let postData = "";
-//         postData += chunk;
-//       });
-//       req.on("end", function() {
-//         console.log("client post");
-//         let postDataObject = JSON.parse(postData);
-//         let userName = postDataObject.user_name;
-//         let password = postDataObject.password;
-//         res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
-//       });
-//     }
-//   }
-// });
-// httpServer.listen(3000);
+server.listen(port, err => {
+  if (err) {
+    throw err;
+  }
+  console.log("serv listening on port " + port);
+  const req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`);
+    res.on("data", d => {
+      process.stdout.write(d);
+    });
+  });
+  req.on("error", err => {
+    throw err;
+  });
+  req.write(data);
+  req.end();
+});
 
 //EXPRESS SOLUTION
-const express = require("express");
-const app = express();
-const port = 3000;
-app.get("/", function(req, res) {
-  res.send("hello");
-});
-let quote = randomQuote.toString();
-app.post("/submit", function(req, res) {
-  res.send(quote);
-});
-
-app.listen(port, () => console.log("server listens on" + port));
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+// app.get("/", function(req, res) {
+//   res.send("hello");
+// });
+// let quote = randomQuote.toString();
+// app.post("/submit", function(req, res) {
+//   res.send(quote);
+// });
